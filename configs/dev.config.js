24c2641a99950af750c399787ee4b00bec5db855
ketan2603 <ketanprajapati245@gmail.com>
2020-04-04 15:14:12 +0530

    new xha

--- configs/dev.config.js
+++ configs/dev.config.js
@@ -1,14 +1,14 @@
 const config = {
     app: {
         port: 4444,
-        host: 'http://sandbox.aavgo.com',
+        host: 'http://192.168.1.24',
         adminHost: 'http://localhost:4444/'
     },
     database: {
         db: 'smp',
         username: 'postgres',
-        password: '12345',
-        host: '192.168.43.128',
+        password: 'ketan123',
+        host: '192.168.43.30',
         port: 5432
     },
 };
