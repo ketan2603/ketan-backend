cfd6c56c78fe70b2b5412e6f2177db0f4b4502d1
saunak-shah <saunakpshah@gmail.com>
2020-06-15 13:37:32 +0530

    add edit update faimily member

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const db = require('../sql')
-const queryBuiler = require('../querybuilders/querybuilder')
+const queryBuiler = require('../querybuilders/querybuilder');
 
 /* GET home page. */
 router.get('/', function (req, res) {
@@ -41,6 +41,33 @@ router.all('/city/getFamilybyCity', function (req, res) {
   const data = req.body;
   db.executeQuery(queryBuiler.getAllFamilyBycity(data), (cities) => {
     res.send(cities);
+  })
+});
+
+router.all('/family/memer/add', function (req, res) {
+  console.log(req.query)
+  console.log("req.body", req.body)
+  const data = req.body;
+  if (!data.family_id) {
+    return res.send({ success: 0, msg: "Invalid data." });
+  }
+  db.executeQuery(queryBuiler.insertFamilyMember(data), (person) => {
+    res.send({ success: 1, msg: "Member add successfully." });
+  })
+});
+
+router.all('/family/memer/update', function (req, res) {
+  console.log(req.query)
+  console.log("req.body", req.body)
+  const data = req.body;
+  if (!data.family_id) {
+    return res.send({ success: 0, msg: "Invalid data." });
+  }
+  if (!data.person_id) {
+    return res.send({ success: 0, msg: "Invalid data." });
+  }
+  db.executeQuery(queryBuiler.updateFamilyMember(data), (person) => {
+    res.send({ success: 1, msg: "Member data updated successfully." });
   })
 });
 
